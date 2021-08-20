549f4e0a848be800ce8fced386fe939baa1fd98b
Masaichi Takai <takai3mobile@gmail.com>
2021-08-20 13:55:48 +0900

    1st commit

---
+++ src/Sample.kt
@@ -0,0 +1,9 @@
+fun main (args: Array<String>) {
+    val message = """
+        |こんにちは。世界！
+        |私は、Kotlinに関して勉強しています。
+        |Kotlinは、とても簡素なプログラミングを書くことができます。
+        |皆さん、一緒にプログラミングしましょう
+        """.trimMargin()
+    println(message)
+}
